1cf3f021009c31f1a4bd6a6f1c3e264698fc6e07
Manisha Moharana <Manisha Moharana@DESKTOP-VDQLJLJ>
2025-03-01 16:44:53 +0530

    remove comments.

--- greenEmart/src/main/java/com/example/greenEmart/service/VegetableService.java
+++ greenEmart/src/main/java/com/example/greenEmart/service/VegetableService.java
@@ -91,7 +91,6 @@ public class VegetableService {
 		return vegRepo.findByName(name);
 	}
 	
-	// Method to Update Vegitable....
 	public String updateVegetable(GreenVeggies vegetable){
 		Optional<GreenVeggies> optionalVegetable=vegRepo.findById(vegetable.getId());
 		
